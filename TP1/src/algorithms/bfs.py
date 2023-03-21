783fe8e57a59426d2075b5a47c8ef5562989f1f7
MateoPerezRivera <matperez@itba.edu.ar>
2023-03-21 12:41:14 -0300

    bfs added nodes to optimize, added graphs

--- TP1/src/algorithms/bfs.py
+++ TP1/src/algorithms/bfs.py
@@ -6,60 +6,85 @@ import pdb
 from src.algorithms.utils.fillFunctions import fill_connected_cells
 from src.algorithms.utils.fillFunctions import check_game_over
 
+class Node:
+    
+    def __init__(self, grid, color, parent):
+        self.grid = grid
+        self.parent = parent
+        self.color = color
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.grid == other.grid
+
+    def __hash__(self):
+        return hash(str(self.grid))
+
+    def getParent(self):
+        return self.parent
+    
+    def getGrid(self):
+        return self.grid
+    
+    def getColor(self):
+        return self.color
+    
+    def getNodeScore(self):
+        visited = set()
+        pending = []
+        pending.append((0,0))
+        scoreCounter = 0 
+        while pending:
+            currentCoord = pending.pop()
+            scoreCounter +=1
+            row,column = currentCoord
+            visited.add((row,column))
+            for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                neighbor_row, neighbor_column = row + i, column + j
+                # Check if the neighboring cell is within the bounds of the grid
+                if (0 <= neighbor_row < len(self.grid)) and (0 <= neighbor_column < len(self.grid[0]) and self.grid[neighbor_row][neighbor_column] == self.color and (neighbor_row,neighbor_column) not in visited):
+                    pending.append((neighbor_row,neighbor_column))
+                
+        return scoreCounter
+
 def fill_zone_bfs(grid,colors): # esta funcion nos genera la grilla inicial, definimos las coordenadas y al color que quiero cambiar.
     # Inicio el tiempo para ver cuanto tarde en procesarlo y hacerlo
     start_time = time.time()
-    
-    solutions = [[]]
-    solutionsAux = []
-    gridAux = []
+    total_time = 0
+    visited = set()
+    pending_nodes = [ Node(grid, grid[0][0],None)]
+    done = False
     nodes_expanded_amount = 0
     nodes_border_amount = 0
+    solution = []
     
-    pdb.set_trace()
-    print(grid)
-    
-    while(1):
-        solutionAux=[]
-        for color in range(colors):
-            for solution in solutions:
-                if(len(solution)==0 or solution[len(solution)-1] != color):
-                    solutionAux.append(solution+[color])
-                    nodes_border_amount += 1
-        print(solutionAux)
-        solutions = copy.deepcopy(solutionAux)
-        print(solutions)
-        for solution in solutions:
-            gridAux = copy.deepcopy(grid)
-            nodes_expanded_amount+=1
-            for color in solution:
-                if color!=gridAux[0][0]:
-                    gridAux = fill_connected_cells(gridAux,color)
-            if(check_game_over(gridAux)):
+    while 1:
+        while(pending_nodes):
+            current_node = pending_nodes.pop()
+            nodes_expanded_amount += 1
+            visited.add(current_node)
+            if check_game_over(current_node.getGrid()):
+                rebuildSolution(current_node,solution)
                 end_time = time.time()
                 total_time = end_time - start_time
-                return solution, total_time, nodes_expanded_amount, nodes_border_amount
-                
-
+                return solution, total_time, nodes_expanded_amount, nodes_border_amount , True
+        nodes_border_amount= nodes_expanded_amount*(colors-1)
+        get_node_succesors(current_node,current_node.getColor(),colors,visited,pending_nodes)
     
-    #target_color = grid[x][y] # target color es el color de la primera celda (verde ponele)
-    #visited = set() # seteamos una lista de celdas visitadas
-    #queue = [(x, y)]
-    #while queue: # va a terminar cuando no haya mas celdas q visitar
-    #    x, y = queue.pop(0)
-    #    if (x, y) in visited: # si ya fue visitada la salteo
-    #        continue
-    #    visited.add((x, y))
-    #    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]): #chequeo que este dentro de la grilla 
-    #        continue
-    #    if grid[x][y] != target_color: # si el color de la celda es distinto a verde (el de la celda inicial) la salteo xq no se puede cambiar al fill color (rojo ponele)
-    #        continue
-    #    grid[x][y] = fill_color # si la celda en la q estoy es del target color (osea verde) la cambio a fill color osea rojo
-    #    queue.append((x+1, y))
-    #    queue.append((x-1, y))
-    #    queue.append((x, y+1))
-    #    queue.append((x, y-1))
-
-    #    return grid
+                
+def rebuildSolution(final_node,solution):
+    current_node = final_node
+    while current_node.getParent() != None:
+        solution.append(current_node.getColor())
+        current_node = current_node.getParent()
+    solution.reverse()
 
 
+def get_node_succesors(parent_node,currentColor,colorAmount,visited,pending_nodes):
+    for color in range(colorAmount):
+        if color != currentColor:
+            gridCopy = copy.deepcopy(parent_node.getGrid())
+            newNode = Node(fill_connected_cells(gridCopy,color),color,parent_node)
+            if parent_node.getNodeScore() < newNode.getNodeScore() and newNode not in visited:
+                pending_nodes.append(newNode)
