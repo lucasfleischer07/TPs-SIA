4ccd23486f7a7c5f0ae7c1f2cd66c12f1de46df0
MalenaBanfi <mabanfi@itba.edu.ar>
2023-05-27 10:28:48 -0300

    adding diferent mutate method
    changing qty of bites

--- TP4/main_hopfield.py
+++ TP4/main_hopfield.py
@@ -3,37 +3,64 @@ import numpy as np
 from src.hopfield import train_hopfield
 from src.hopfield import print_letter
 from src.hopfield import mutate
+from src.hopfield import mutate_pattern
 
+
+
+# def parse_file():
+#     file = open('docs/alphabet_letters.txt', 'r')
+#     lines = file.readlines()
+    
+#     letters = []
+#     letter = []
+    
+#     for line in lines:
+#         if line[0] != "-" and line[0] != "1":
+#             letters.append(letter)
+#             letter = []
+#         else:
+#             i = 0
+#             while i < len(line):
+#                 if line[i] == "1":
+#                     letter.append(1.)
+#                     i += 1
+#                 elif line[i] == "-":
+#                     letter.append(-1.)
+#                     i += 2
+#                 else:
+#                     i += 1
+#     return letters
 
 def parse_file():
-    file = open('docs/alphabet_letters.txt', 'r')
+    file = open('docs/letters.txt', 'r')
     lines = file.readlines()
-    
+
     letters = []
-    letter = []
-    
-    for line in lines:
-        if line[0] != "-" and line[0] != "1":
-            letters.append(letter)
-            letter = []
-        else:
-            i = 0
-            while i < len(line):
-                if line[i] == "1":
-                    letter.append(1.)
-                    i += 1
-                elif line[i] == "-":
-                    letter.append(-1.)
-                    i += 2
-                else:
-                    i += 1
+
+    for i in range(26):
+        letter = []
+        for j in range(5):
+            current_line = list(map(lambda v: int(v), lines[j + i * 5].split()))
+            for n in current_line:
+                letter.append(n)
+        letters.append(letter)
+
+    letters = np.array(letters)
+    letters = np.where(letters == 0, -1, letters)
+
     return letters
 
 
 def main():
     alg_name = "hopfield"
-    letters = parse_file()
-    train_letters = np.array([letters[17], letters[19], letters[21], letters[22]]) #R,T,V,W
+    letters = parse_file() 
+    print(len(letters))
+
+    train_letters = np.array([letters[0], letters[11], letters[19], letters[21]]) #A,L,T,V.
+
+    # train_letters = np.array([letters[5], letters[12], letters[21], letters[25]]) #A,L,T,V. fmvz y ajlx
+    # train_letters = np.array([letters[0], letters[9], letters[11], letters[23]]) #A,L,T,V. fmvz y ajlx (mejor)
+
 
     print("\nInput letters: \n")
     for input in train_letters:
@@ -42,16 +69,23 @@ def main():
 
     print("\n ------------------------- \n")
 
-    letter_with_noise = mutate(letters[22], 0.4)
-    # predict_letter = letters[6]
+    # letter_with_noise = mutate(letters[0], 0.1)
+    letter_with_noise = mutate_pattern(letters[0], 3)
+    # predict_letter = letters[0]
+
+    print("Letter to predict with noise in iteration 0: \n")
+    print_letter(letter_with_noise)
     
     letter_solved, iterations = train_hopfield(np.array(letter_with_noise), train_letters)
+    # letter_solved, iterations = train_hopfield(predict_letter, train_letters)
+
     # print("Letter to predict: \n")
     # print_letter(predict_letter)
-    print("Letter to predict with noise: \n")
-    print_letter(letter_with_noise)
-    print("\nLetter predicted: \n")
+    # print("Letter to predict with noise: \n")
+    # print_letter(letter_with_noise)
+    print("\nLetter predicted in " + str(iterations) + " iteration/s \n" )
     print_letter(letter_solved)
+    print("\n")
 
 
 if __name__ == "__main__":
--- TP4/src/hopfield.py
+++ TP4/src/hopfield.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 
 def train_hopfield(query, initial_letters): #le pasa la letra q quiere llegar, y las 4 letras de input
@@ -15,7 +16,10 @@ def train_hopfield(query, initial_letters): #le pasa la letra q quiere llegar, y
         S = query #patron de consulta, S es el vector de estados
         old_S = None
 
-        while not np.array_equal(S, old_S): 
+        while not np.array_equal(S, old_S):
+            if(iterations != 0):
+                print("\nIteration: " + str(iterations) + "\n") 
+                print_letter(S)
             old_S = S
             S = np.sign(np.dot(weights, S)) 
             iterations += 1 #iteraciones
@@ -43,3 +47,11 @@ def mutate(letter, prob): #funciona como que 0.1 es menor que 0.9.
             mutated_letter[i] *= -1
     return mutated_letter
 
+
+def mutate_pattern(pattern, bytes_to_change):
+    indexs = np.random.choice(len(pattern), bytes_to_change, replace=False)
+    p = deepcopy(pattern)
+    for i in indexs:
+        p[i] = 1 if p[i] == -1 else -1
+
+    return p
